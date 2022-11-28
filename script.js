960ae60cee9f2a5748de2e9460cd3bc6454f111e
Ronit N <ronit@nayak.ca>
2022-11-28 16:32:52 -0500

    got the date working

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
-var myDate = new Date();
-
+/*
 const time = document.getElementById("time")
+time.addEventListener("input", timeFunction)
+function timeFunction() {
+  time.innerText = currentTime.value + ""
+}
 
-time.addEventListener("input", )
+hour, minute, second, am/pms
 
-document.getElementById("date").innerHTML = new Date();
+day, month, daynumber, year
+*/
+
+var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+var myDate = new Date();
+    month = myDate.getMonth();
+    date = myDate.getDate();
+    day = myDate.getDay();
+    year = myDate.getFullYear();
+
+var monthprint = months[month];
+var dayprint = days[day];
+
+document.getElementById("date").innerHTML = dayprint + ", " + monthprint +" "+ date +" "+ year;
