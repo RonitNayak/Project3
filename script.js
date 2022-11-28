554031d96e429665ef34cb958175337517123f6b
Ronit N <ronit@nayak.ca>
2022-11-28 17:52:14 -0500

    I need to learn how to use functions now

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-/*
-const time = document.getElementById("time")
-time.addEventListener("input", timeFunction)
-function timeFunction() {
-  time.innerText = currentTime.value + ""
-}
-
-HR:MN:SC PM
-
-*/
-
 var myDate = new Date();
     hours = myDate.getHours();
     minutes = myDate.getMinutes();
@@ -23,6 +12,54 @@ var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "S
 
 var monthprint = months[month];
 var dayprint = days[day];
+var hrprevious = "0";
+var mnprevious = "0";
+var scprevious = "0";
+var ordinal = "";
+var ampm = "";
 
-document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds;
-document.getElementById("date").innerHTML = dayprint + ", " + monthprint +" "+ date +" "+ year;
+if (date== 1) {
+  ordinal = "st"
+} else if (date==2) {
+  ordinal = "nd"
+} else if (date==3) {
+  ordinal = "rd"
+} else if (date==21) {
+  ordinal = "st"
+} else if (date==22) {
+  ordinal = "nd"
+} else if (date==23) {
+  ordinal = "rd"
+} else if (date==31) {
+  ordinal = "st"
+} else {
+  ordinal = "th"
+}
+
+if (hours<12) {
+  ampm = "AM"
+} else {
+  (hours=hours-12), ampm = "PM";
+}
+
+if (hours.length<2) {
+  hrprevious = "0"
+} else {
+  hrprevious = ""
+}
+
+if (minutes.length<2) {
+  mnprevious = "0"
+} else {
+  mnprevious = ""
+}
+
+if (seconds.length<2) {
+  scprevious = "0"
+} else {
+  scprevious = ""
+}
+
+document.getElementById("time").innerHTML = hrprevious + hours + ":" + mnprevious + minutes + ":" + scprevious + seconds + " " + ampm;
+document.getElementById("date").innerHTML = dayprint + ", " + monthprint +" "+ date + ordinal + " "+ year;
+
